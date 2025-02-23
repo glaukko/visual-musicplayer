87eea2164e7c8ba809173ee938c2a37827a5209a
glaukko <136825566+glaukko@users.noreply.github.com>
2025-02-23 05:14:30 -0300

    Delete song.py

--- song.py
+++
@@ -1,7 +0,0 @@
-class Song():
-    def __init__(self, fileid, filename, duration, coverImg = None):
-        self.fileid = fileid
-        self.filename = filename
-        
-        self.coverImg = coverImg
-        self.duration = duration #(hours, minutes, seconds)
